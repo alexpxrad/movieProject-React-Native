6c2235afe0d85cc9778b82666de87aa8eca9c134
alexpxrad <alexpxrad@gmail.com>
2023-01-19 20:01:39 -0500

    fixed movie title and genre in detail screen

--- screens/Detail.js
+++ screens/Detail.js
@@ -1,5 +1,6 @@
+import { style } from 'deprecated-react-native-prop-types/DeprecatedViewPropTypes';
 import React, {useEffect, useState}from 'react'
-import {ScrollView, StyleSheet, Image, Dimensions, ActivityIndicator} from 'react-native'
+import {ScrollView, StyleSheet, Image, Dimensions, ActivityIndicator, Text, View} from 'react-native'
 import {getMovie} from '../services/services'
 
 
@@ -23,16 +24,30 @@ const Detail = ({route, navigation}) => {
     return (
         <React.Fragment>
            {loaded &&  
-           (<ScrollView>
+           (<ScrollView >
              <Image 
              resizeMode='cover'
              style={styles.image} 
                 source={
                     movieDetail.poster_path 
-                    ? {uri: 'https://image.tmdb.org/t/p/w500' + movieDetail.poster_path}
+                    ? {
+                        uri: 
+                            'https://image.tmdb.org/t/p/w500' 
+                            + movieDetail.poster_path,
+                        }
                      : placeHolderImage
                     }
                 />
+                <View  style={styles.container}>
+                <Text style={styles.movieTitle} >{movieDetail.title}</Text> 
+                {movieDetail.genres && (
+                    <View  style={styles.genresContainer}>
+                    {movieDetail.genres.map(genre => {
+                         return <Text style={styles.genre} key={genre.id}>{genre.name}</Text>     
+                    })}
+                       </View>
+                )}
+                </View>
             </ScrollView>
             )}
             {!loaded && <ActivityIndicator size="large" />}
@@ -41,11 +56,31 @@ const Detail = ({route, navigation}) => {
 }
 
 const styles = StyleSheet.create({
-   
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
     image : {
         height: height / 2.5,
       
     },
+    movieTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 10,
+        marginBottom: 10,
+    },
+    genresContainer: {
+        flexDirection: 'row',
+        alignContent : 'center',
+        marginTop: 20,
+    },
+    genre: {
+        marginRight : 10,
+        fontWeight: 'bold',
+
+    }
 });
 
 
